aef4a35856368f5252abec0a2658936483a443f9
timvan <timvanellemeet@gmail.com>
2018-02-03 01:59:20 +1100

    total counter added

--- kpis/urls.py
+++ kpis/urls.py
@@ -3,5 +3,6 @@ from. import views
 
 urlpatterns = [
 	path('', views.index, name = 'index'),
+	path('<int:pk>/log_activity', views.log_activity, name = 'log_activity'),
 
 ]
--- kpis/views.py
+++ kpis/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 
 from .models import KPI, activity
@@ -15,3 +15,8 @@ def index(request):
 	return render(request, 'kpis/index.html', context)
 
 
+def log_activity(response, pk):
+	kpi = get_object_or_404(KPI, pk = pk)
+	new_activity = activity(kpi = kpi)
+	new_activity.save()
+	return redirect('index')
